6593fb3bb355c99c1b07469152af2bf47fc3027c
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-09-04 18:35:43 +0600

    connect mysql successfully

--- app.js
+++ app.js
@@ -12,6 +12,17 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+const con = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+});
+
+con.connect((err, connection) => {
+    if (err) throw err;
+    console.log('database connected successfully!');
+});
 
 app.get('/', (req, res) => {
     res.send('Welcome To HR Cloud Server!!!')

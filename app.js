065f0b26b061a7eb0e11cb28b8a49674a1a86a43
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-09-05 21:25:12 +0600

    data insert into employee table

--- app.js
+++ app.js
@@ -12,17 +12,25 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const con = mysql.createConnection({
+const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME
 });
 
-con.connect((err, connection) => {
+db.connect((err, connection) => {
     if (err) throw err;
     console.log('database connected successfully!');
 });
+
+// data insert into employee table
+app.get('/', (req, res) => {
+    const sqlInsert = "INSERT INTO `employee` (`id`, `first_name`, `last_name`, `email`, `phone`) VALUES (NULL, 'sb', 'sk', 's@bb', '100');";
+    db.query(sqlInsert, (err, result) => {
+        res.send("hello")
+    })
+})
 
 app.get('/', (req, res) => {
     res.send('Welcome To HR Cloud Server!!!')
